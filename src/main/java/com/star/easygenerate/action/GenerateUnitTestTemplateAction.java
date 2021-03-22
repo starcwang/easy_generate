7c7760f808d99c72abc9857b6cc2a434ea752c7a
wangchao <760823254@qq.com>
2021-03-22 23:39:05 +0800

    1.10-unit test

--- src/main/java/com/star/easygenerate/action/GenerateUnitTestTemplateAction.java
+++ src/main/java/com/star/easygenerate/action/GenerateUnitTestTemplateAction.java
@@ -2,9 +2,12 @@ package com.star.easygenerate.action;
 
 import java.io.BufferedInputStream;
 import java.io.InputStreamReader;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import com.google.common.collect.Maps;
 import com.intellij.ide.fileTemplates.FileTemplateUtil;
@@ -23,16 +26,23 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.ClassFileViewProvider;
 import com.intellij.psi.FileViewProvider;
 import com.intellij.psi.JavaDirectoryService;
+import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiJavaFile;
 import com.intellij.psi.PsiManager;
+import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiPackage;
+import com.intellij.refactoring.classMembers.MemberInfoBase;
+import com.intellij.refactoring.util.classMembers.MemberInfo;
 import com.intellij.testIntegration.createTest.CreateTestAction;
 import com.intellij.testIntegration.createTest.CreateTestDialog;
 import com.intellij.util.ResourceUtil;
 import com.star.easygenerate.dialog.CreateUnitTestDialog;
+import org.apache.commons.compress.utils.Lists;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.jps.model.java.JavaSourceRootType;
 
 /**
@@ -57,18 +67,42 @@ public class GenerateUnitTestTemplateAction extends AnAction {
         PsiPackage srcPackage = JavaDirectoryService.getInstance().getPackage(srcDir);
         final Module srcModule = ModuleUtilCore.findModuleForPsiElement(psiJavaFile);
 
+        //属性
+        PsiClass psiClass = psiJavaFile.getClasses()[0];
+
+
+
         CreateUnitTestDialog dialog = new CreateUnitTestDialog(project, "xxxxxxx", psiJavaFile.getClasses()[0], srcPackage, srcModule);
         if (!dialog.showAndGet()) {
             return;
         }
+        PsiField[] allFields = psiJavaFile.getClasses()[0].getAllFields();
+        Map<String, String> fieldMap = Maps.newHashMap();
+        for (PsiField field : allFields) {
+            if (field.getAnnotation("javax.annotation.Resource") != null || field.getAnnotation("javax.annotation.Resource") != null) {
+                fieldMap.put(field.getType().getCanonicalText(), field.getName());
+            }
+        }
+
+        Collection<MemberInfo> selectedMemberList = dialog.getSelectedMethods();
+        List<PsiMethod> methodList = selectedMemberList.stream()
+            .map(memberInfo -> (PsiMethod)memberInfo.getMember()).collect(Collectors.toList());
+
         PsiDirectory tgtDir = dialog.getTargetDirectory();
 
         psiJavaFile.getClasses();
+        String className = psiJavaFile.getClasses()[0].getName();
+        if (StringUtils.isBlank(className)) {
+            return;
+        }
 
         Map<String, Object> params = Maps.newHashMap();
         params.put("user", "cc");
-        params.put("className", psiJavaFile.getClasses()[0].getName());
+        params.put("className", className);
+        params.put("instanceName", StringUtils.substring(className, 0, 1).toLowerCase() + StringUtils.substring(className, 1));
         params.put("testClassName", dialog.getClassName());
+        params.put("fieldMap", fieldMap);
+        params.put("methodList", methodList);
 
         try {
             String text = ResourceUtil.loadText(ResourceUtil.getResource(this.getClass().getClassLoader(), "template", "unit-test.vm"));
@@ -80,6 +114,7 @@ public class GenerateUnitTestTemplateAction extends AnAction {
         } catch (Exception exception) {
             exception.printStackTrace();
         }
+
         //
         //CommandProcessor.getInstance().executeCommand(project, () -> {
         //    final TestGenerator generator = TestGenerators.INSTANCE.forLanguage(JavaLanguage.INSTANCE);
